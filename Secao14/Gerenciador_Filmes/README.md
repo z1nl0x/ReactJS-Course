e17a5955f66741747df4c41a8432ec7691d70f35
z1nl0x <paulo.kreft@gmail.com>
2022-10-20 19:39:05 -0300

    Section 14 finalized, added readme, and updated some titles on html files of other projects

---
+++ Secao14/Gerenciador_Filmes/README.md
@@ -0,0 +1,15 @@
+# Seção 14 "Enviando requisições HTTP (Conectando-se à um banco de dados)" - Projeto Gerenciador de Filmes
+
+### É preciso ter a versão LTS do NodeJS instalado na máquina antes de todo o procedimento daqui descrito - ultima versão LTS até o momento da finalização deste projeto é (16.17.0)
+
+- Acesse para fazer o download e a instalação do mesmo no endereço: https://nodejs.org/
+
+#### Instalação e Configuração do Projeto para rodar direto na tua máquina
+
+- Baixe os arquivos e extraia a pasta Gerenciador_Filmes.
+
+- No mesmo diretório "Gerenciador_Filmes" abra o terminal/prompt de comandos e digite: npm install (Para instalar as dependências do projeto).
+
+#### Inicializando a Aplicação Gerenciador de Filmes
+
+- No mesmo diretório "Gerenciador_Filmes" abra o terminal/prompt de comandos e digite: npm start (Para inicializar a aplicação).
---
+++ Secao14/Gerenciador_Filmes/src/App.js
@@ -0,0 +1,85 @@
+import React, { useState, useEffect, useCallback } from "react";
+
+import MoviesList from "./components/MoviesList";
+import AddMovie from "./components/AddMovie";
+import "./App.css";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchMoviesHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(
+        "https://react-http-17ddb-default-rtdb.firebaseio.com/movies.json"
+      );
+      if (!response.ok) {
+        throw new Error("Algo deu errado!");
+      }
+
+      const data = await response.json();
+
+      const loadedMovies = [];
+
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+
+      setMovies(loadedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  async function addMovieHandler(movie) {
+    const response = await fetch(
+      "https://react-http-17ddb-default-rtdb.firebaseio.com/movies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
+
+  let content = <p>Filmes não encontrados.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Carregando...</p>;
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
+      <section>
+        <button onClick={fetchMoviesHandler}>Buscar Filmes</button>
+      </section>
+      <section>{content}</section>
+    </React.Fragment>
+  );
+}
+
+export default App;
