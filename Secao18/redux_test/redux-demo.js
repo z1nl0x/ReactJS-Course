5b99137bfa267e442c715f6c890fe78e39e80661
z1nl0x <paulo.kreft@gmail.com>
2022-11-07 17:24:21 -0300

    Section 18 initialized

---
+++ Secao18/redux_test/redux-demo.js
@@ -0,0 +1,34 @@
+// To run this file, install the dependencies and then run on
+// the terminal: node redux-demo.js
+
+const redux = require("redux");
+
+const defaultValue = { counter: 0 };
+
+const counterReducer = (state = defaultValue, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+
+  return state;
+};
+
+const store = redux.createStore(counterReducer);
+
+const counterSubscriber = () => {
+  const latestState = store.getState();
+  console.log(latestState);
+};
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({ type: "increment" });
+store.dispatch({ type: "decrement" });
